575c1a15f1e7dc0488d9cab3c4bb15bb1a669296
Elad Saar <saarelad922@gmail.com>
2024-05-08 01:53:03 +0300

    Some styling and cleanup

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,22 +1,25 @@
+'use client'
 import Link from 'next/link';
+import {usePathname} from "next/navigation";
 
 export const NavBar = () => {
+    const pathname = usePathname();
     return (
-        <nav className="navbar navbar-expand navbar-dark bg-primary d-flex justify-content-between">
+        <nav className="navbar navbar-expand navbar-dark bg-dark d-flex justify-content-between">
             <div>
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link href="/" className="nav-link">
+                    <li className="nav-item">
+                        <Link href="/" className={pathname === "/" ? "active nav-link" : "nav-link"}>
                             Home
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link href="/movies" className="nav-link">
+                        <Link href="/movies" className={pathname.includes("/movies") ? "active nav-link" : "nav-link"}>
                             Movies
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link href="/about" className="nav-link">
+                        <Link href="/about" className={pathname.includes("/about") ? "active nav-link" : "nav-link"}>
                             About
                         </Link>
                     </li>
--- app/movies/[movieId]/page.tsx
+++ app/movies/[movieId]/page.tsx
@@ -4,6 +4,9 @@ import FETCH_OPTIONS from "@/app/_lib/fetch-config";
 import {getPosterUrl} from "@/app/_lib/poster-url";
 import {getImageConfig} from "@/app/_lib/image-config-api";
 import Image from "next/image";
+import Link from "next/link";
+import { TiArrowBack } from "react-icons/ti";
+
 
 interface Props {
     params: { movieId: string }
@@ -17,13 +20,17 @@ const MovieDetailsPage = async ({params}: Props) => {
     const imageConfig = await getImageConfig();
     const posterUrl = getPosterUrl(movie.poster_path, imageConfig);
     return (
-        <div className='d-flex gap-5'>
-            <Image width={500} height={400} src={posterUrl} alt={movie?.title}/>
-            <div className='d-flex flex-column gap-5 w-50'>
-                <h1>{movie?.title}</h1>
-                <p>{movie?.overview}</p>
-                <div>Released: {movie?.release_date}</div>
-                <div className='d-flex flex-row gap-3'>{movie?.genres.map(genre => <span key={genre.id}>#{genre?.name}</span>)}</div>
+        <div className="m-2">
+            <Link href="/movies" className="d-flex gap-3 my-2"><TiArrowBack/><span>Back to list</span></Link>
+            <div className='d-flex gap-5'>
+                <Image width={500} height={400} src={posterUrl} alt={movie?.title}/>
+                <div className='d-flex flex-column gap-5 w-50'>
+                    <h1>{movie?.title}</h1>
+                    <p>{movie?.overview}</p>
+                    <div>Released: {movie?.release_date}</div>
+                    <div className='d-flex flex-row gap-3'>{movie?.genres.map(genre => <span
+                        key={genre.id}>#{genre?.name}</span>)}</div>
+                </div>
             </div>
         </div>
     );
